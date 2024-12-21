cdf774d98f09f40a13e7828cc7281c73fca55879
Nuwantha005 <nuwanthakumara999@gmail.com>
2024-12-21 23:14:11 +0530

    Add utility functions, layout enhancements, and Tailwind CSS configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,45 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+"use client";
+
 import "./globals.css";
+import FlickeringGrid from "@/components/ui/flickering-grid";
+import { useEffect, useState } from "react";
+
+function Background({ children }: { children: React.ReactNode }) {
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
+
+  useEffect(() => {
+    const updateWidthHeight = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+      console.log(window.innerWidth);
+    };
+
+    updateWidthHeight(); // Initial update
+    window.addEventListener("resize", updateWidthHeight);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("resize", updateWidthHeight);
+    };
+  }, []);
+
+  return (
+    <>
+      {children}
+      <FlickeringGrid
+        className="flex z-0 absolute inset-0 size-full overflow-hidden"
+        squareSize={50}
+        gridGap={5}
+        color="#6B72ff"
+        maxOpacity={0.2}
+        flickerChance={0.5}
+        width={windowWidth}
+        height={windowHeight}
+      />
+    </>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -9,7 +48,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Background>{children}</Background>
+      </body>
     </html>
   );
 }
