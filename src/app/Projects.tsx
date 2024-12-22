1d9f6af5578ca34a24a6758dcd34592ba85dd110
Nuwantha005 <nuwanthakumara999@gmail.com>
2024-12-22 11:47:42 +0530

    Add masonry layout for Projects page and implement ProjectCard component

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ProjectCard from "@/components/projects/ProjectCard";
+import "./globals.css";
 
 const Projects: React.FC = () => {
+  interface Project {
+    image: string;
+    name: string;
+    description: string;
+    tags: string[];
+  }
+
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    fetch("/jason/projectDetails.json")
+      .then((res) => res.json())
+      .then((data) => setProjects(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <h1>Projects</h1>
+    <div className="relative z-10 p-2 ">
+      <div className="masonry-grid">
+        {projects.map((project, index) => (
+          <div key={index} className="masonry-item">
+            <ProjectCard
+              image={project.image}
+              name={project.name}
+              description={project.description}
+              tags={project.tags}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/projects/ProjectCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import FloatingSection from "../ui/FloatingSection";
+
+interface ProjectCardProps {
+  image: string;
+  name: string;
+  description: string;
+  tags: string[];
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  image,
+  name,
+  description,
+  tags,
+}) => {
+  return (
+    <div className="project-card transition-transform duration-300 ease-in-out transform hover:scale-105">
+      <FloatingSection>
+        <div className="flex flex-col items-center">
+          <img
+            src={image}
+            alt={name}
+            className="w-full object-cover rounded-lg dark:bg-gray-800/50 bg-gray-200/50  p-2 shadow-md"
+          />
+          <h3 className="text-lg font-semibold mt-4">{name}</h3>
+          <p className="dark:text-gray-100 text-gray-800 mt-2 text-center">
+            {description}
+          </p>
+          <div className="mt-4 flex-row justify-center space-x-2">
+            {tags.map((tag, index) => (
+              <span
+                key={index}
+                className="bg-gray-800 text-gray-200 dark:bg-gray-200 dark:text-gray-800 px-3 py-1 rounded-full text-sm"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+      </FloatingSection>
+    </div>
+  );
+};
+
+export default ProjectCard;
