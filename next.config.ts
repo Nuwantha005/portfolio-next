537abcbd774d1f844c5b22a98adb535cf84e0ad5
Nuwantha005 <nuwanthakumara999@gmail.com>
2024-12-22 23:00:39 +0530

    Update Next.js configuration for static export and add export script in package.json

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // filepath: /d:/Documents/Web Development/React Projects/portfolio-next/next.config.js
+};
+
+module.exports = {
+  output: "export",
 };
 
 export default nextConfig;
