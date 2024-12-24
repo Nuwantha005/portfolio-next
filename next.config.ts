5f3147f35704fc46c1f0c618f9fa15d85072a0e1
Nuwantha Kumara <nuwanthakumara999@gmail.com>
2024-12-24 10:14:26 +0530

    Refactor Next.js configuration to improve export handling and update module exports

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  // filepath: /d:/Documents/Web Development/React Projects/portfolio-next/next.config.js
-  output: "export",
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   // filepath: /d:/Documents/Web Development/React Projects/portfolio-next/next.config.js
+//   output: "export",
+// };
 
 module.exports = {
   //output: "export",
@@ -12,5 +12,13 @@ module.exports = {
     unoptimized: true,
   },
 };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+};
+
+module.exports = nextConfig;
+
 
 export default nextConfig;
